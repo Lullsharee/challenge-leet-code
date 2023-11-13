3706d1e1b964e9318efbe4ebd0cbed52d5342822
helloooooo <yomicerisier@gmail.com>
2023-11-13 21:12:13 +0900

    hash map use

--- src/two_sum.cpp
+++ src/two_sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
@@ -10,6 +11,17 @@ vector<int> twoSum(vector<int>& nums, int target) {
       if (nums[i] + nums[j] == target) return {i, j};
     }  
   }
+  return {};
+}
+
+vector<int> twoSumByHash(vector<int>& nums, int target) {
+  unordered_map<int, int> map;
+  for (int i = 0; i < nums.size(); ++i) map[nums[i]] = i;
+  for (int i = 0; i < nums.size(); ++i) {
+    int comp = target - nums[i];
+    if(map.find(comp) != map.end() && map[comp] != i) return {i, map[comp]};
+  }
+  return {};
 }
 
 int main() {
@@ -19,6 +31,7 @@ int main() {
   for(int i = 0; i < N; ++i) cin >> a[i];
   int target;
   cin >> target;
-  vector<int> res = twoSum(a, target);
+  // vector<int> res = twoSum(a, target);
+  vector<int> res = twoSumByHash(a, target);
   cout << "[" << res[0] << "," << res[1] << "]" << endl;
 }
