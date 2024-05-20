910d5ccd034a264ef412645465160b68c1d4c0c3
helloooooo <yomicerisier@gmail.com>
2024-05-20 21:39:04 +0900

    finish pointer

---
+++ src/leetcode/11.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxArea(vector<int>& height) {
+  int ans = 0;
+
+    int left = 0, right = height.size() - 1;
+    while (left != right) {
+      int current_area = min(height[left], height[right]) * (right - left);
+      if (current_area > ans) ans = current_area;
+      if (height[left] < height[right]) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  return ans;
+}
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> heights(N);
+  for (int i = 0; i < N; i++) cin >> heights.at(i);
+  int ans = maxArea(heights);
+}
