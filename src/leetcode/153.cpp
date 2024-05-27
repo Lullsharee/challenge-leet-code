9eae8ed7838011aaeac0916b533b3d5bbd870432
helloooooo <yomicerisier@gmail.com>
2024-05-27 20:45:44 +0900

    fast answer

---
+++ src/leetcode/153.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int findMin(vector<int>& nums) {
+  int left = 0, right = nums.size() - 1;
+  while(left < right) {
+
+    int mid = (left + right) /2;
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return nums[left];
+}
+
+int main() {
+}
